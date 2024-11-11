6afcf51c16b91ea11434b3c6f41f892d31ad2476
Peber <petercort@gmail.com>
2024-11-11 10:37:25 -0500

    updating stuff! [no ci]

---
+++ .github/scripts/pr-scripts/parse-add-labels.js
@@ -0,0 +1,142 @@
+module.exports = async ({github, context, core}) => {
+    const { owner, repo } = context.repo;
+    const { number: issue_number } = context.issue;
+  
+    const body = context.payload.pull_request.body;
+    const releaseTypes = ["major", "minor", "patch", "transition", "no release"];
+    const changeTypes = ["feature", "enhancement", "fix", "bugfix", "chore"];
+    const qualityTypes = ["alpha", "beta"];
+  
+    const releaseMap = {
+      'major': 'semver:major',
+      'minor': 'semver:minor',
+      'patch': 'semver:patch',
+      'transition': 'transition',
+      'no release': 'no-release',
+    };
+    const changeMap = {
+      'feature': 'feature',
+      'enhancement': 'enhancement',
+      'bugfix': 'bugfix',
+      'fix': 'bugfix',
+      'chore': 'chore',
+    };
+  
+    const failedChecks = [];
+  
+    const releaseTypeSection = body.split('## What type of Release is this?')[1]?.split('##')[0]?.trim();
+    if (!releaseTypeSection) {
+      failedChecks.push("No release type section found, please ensure you have updated your pull request with the correct release type.");
+    }
+  
+    const providerVersionSection = body.split('### (If Applicable) Specify Initial Provider Version')[1]?.split('###')[0]?.trim();
+  
+    const changeTypeSection = body.split('## What type of change was made?')[1]?.split('##')[0]?.trim();
+    if (!changeTypeSection) {
+      failedChecks.push("No change type section found, please ensure you have updated your pull request with the correct change type.");
+    }
+    const qualityTypeSection = body.split('## What type of Pre-Release is this?')[1]?.split('##')[0]?.trim();
+  
+    // Find all matches
+    const releaseMatch = releaseTypes.filter(type => new RegExp(`^-\\s*${type}`, 'mi').test(releaseTypeSection));
+    console.log('Matched Release Types:', releaseMatch);
+    const changeMatch = changeTypes.filter(type => new RegExp(`^-\\s*${type}`, 'mi').test(changeTypeSection));
+    console.log('Matched Change Types:', changeMatch);
+  
+    if (releaseMatch.length > 1) {
+      failedChecks.push(`Pull request must have exactly one release label, found ${releaseMatch.join(', ')}.`);
+    }
+  
+    if (changeMatch.length > 1) {
+      failedChecks.push(`Pull request must have exactly one change label, found ${changeMatch.join(', ')}.`);
+    }
+  
+    // Define releaseLabel using releaseMap
+    const releaseLabel = releaseMatch.length === 1 ? releaseMap[releaseMatch[0]] : null;
+    const changeLabel = changeMatch.length === 1 ? changeMap[changeMatch[0]] : null;
+  
+    const labelsToAdd = [
+      releaseLabel,
+      changeLabel
+    ].filter(Boolean);
+  
+    const providerVersionLabel = "provider-version-specified";
+    if (providerVersionSection) {
+      const providerVersionMatch = providerVersionSection.match(/^- (N\/A|\d+\.\d+\.\d+)$/mi);
+      console.log('Matched Provider Version:', providerVersionMatch[1]);
+      if (providerVersionMatch) {
+        if (providerVersionMatch && providerVersionMatch.length === 2) {
+          const version = providerVersionMatch[1];
+          if (version !== 'N/A') {
+            labelsToAdd.push(providerVersionLabel);
+            console.log('Adding provider version:', version);
+            core.setOutput('provider-version', version);
+          }
+        } else {
+          failedChecks.push("Provider version section must contain exactly one item, either a release version (e.g., 1.0.0) or N/A.");
+        }
+      }
+    };
+  
+    if (qualityTypeSection){
+      const qualityLabel = qualityTypes.filter(type => new RegExp(`^-\\s*${type}`, 'mi').test(qualityTypeSection));
+      console.log('Matched Pre-release Types:', qualityLabel);
+      if (qualityLabel.length > 1) {
+        failedChecks.push(`Pull request must have exactly one quality label, found ${qualityLabel.join(', ')}.`);
+      } else if (qualityLabel.length == 1) {
+        labelsToAdd.push(qualityLabel[0]);
+        core.setOutput('prerelease-type', qualityLabel);
+      }
+    };
+  
+    if (failedChecks.length > 0) {
+      const commentBody = `### Label Check Failures\n\n${failedChecks.map(check => `- ${check}`).join('\n')}`;
+      const { data: allComments } = await github.rest.issues.listComments({ issue_number, owner, repo });
+      const existingComment = allComments.find(comment => comment.user.login === 'github-actions[bot]' && comment.body.startsWith('### Label Check Failures'));
+  
+      if (existingComment) {
+        await github.rest.issues.updateComment({
+          owner,
+          repo,
+          comment_id: existingComment.id,
+          body: commentBody
+        });
+      } else {
+        await github.rest.issues.createComment({
+          owner,
+          repo,
+          issue_number,
+          body: commentBody
+        });
+      }
+      core.setFailed("Label checks failed. See the comment for details.");
+      return;
+    }
+  
+    const currentLabels = (await github.rest.issues.listLabelsOnIssue({
+      owner,
+      repo,
+      issue_number
+    })).data.map(label => label.name);
+  
+    const labelsToRemove = [...Object.values(releaseMap), ...Object.values(changeMap), ...qualityTypes, ...providerVersionLabel].filter(label => currentLabels.includes(label) && !labelsToAdd.includes(label));
+  
+    for (const label of labelsToRemove) {
+      await github.rest.issues.removeLabel({
+        owner,
+        repo,
+        issue_number,
+        name: label
+      });
+    }
+    console.log('Adding labels:', labelsToAdd);
+    await github.rest.issues.addLabels({
+      owner,
+      repo,
+      issue_number,
+      labels: labelsToAdd
+    });
+  
+    core.setOutput('release-type', releaseLabel);
+    core.setOutput('change-type', changeLabel);
+  }
---
+++ .github/scripts/pr-scripts/release_draft.js
@@ -0,0 +1,54 @@
+module.exports = async ({github, context, core}) => {
+    const { releaseversion, changelog } = process.env;
+    const { owner, repo } = context.repo;
+    const { number: issue_number } = context.issue;
+          
+    const {data: allComments} = await github.rest.issues.listComments({ issue_number, owner, repo });
+    const ourComments = allComments
+      .filter(comment => comment.user.login === 'github-actions[bot]')
+      .filter(comment => comment.body.startsWith('Created Draft'));
+    
+    const latestComment = ourComments.slice(-1)[0];
+  
+    if (latestComment) {
+      var releaseID = latestComment.body.substring(    
+        latestComment.body.indexOf("`") + 1, 
+        latestComment.body.lastIndexOf("`")
+      );
+      console.log(`Draft Release exists, checking if changes to existing release ID \`${releaseID}\`.`);
+      const {data: lastRelease} = await github.rest.repos.getRelease({
+        owner: owner,
+        repo: repo,
+        release_id: releaseID });
+      if (lastRelease.body == changelog && lastRelease.name == releaseversion){
+          console.log('No changes to the release version or notes of existing release', lastRelease.name, 'with release ID ', releaseID);
+          return;
+      };
+      console.log('Found changes to changelog, deleting existing release ID ', releaseID);
+      const {response} = await github.rest.repos.deleteRelease({
+        owner: owner,
+        repo: repo,
+        release_id: releaseID });
+    };
+  
+    const {data: newRelease}  = await github.rest.repos.createRelease({
+      draft: true,
+      generate_release_notes: false,
+      name: releaseversion,
+      owner: owner,
+      prerelease: false,
+      repo: repo,
+      tag_name: releaseversion,
+      body: changelog });
+  
+    console.log('Created release id', newRelease.id);
+    let finalBody =`Created Draft Release **${newRelease.name}** with ID \`${newRelease.id}\``;
+    const {data: newComment} = await github.rest.issues.createComment({ issue_number, owner, repo, body: finalBody });
+    console.log('Posted comment', newComment.id, '@', newComment.html_url);
+    // Delete all our previous comments
+    for (const comment of ourComments) {
+      if (comment.id === newComment.id) continue;
+      console.log('Deleting previous PR comment from', comment.created_at);
+      await github.rest.issues.deleteComment({ comment_id: comment.id, owner, repo });
+    }
+  }
